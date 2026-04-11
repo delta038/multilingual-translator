cc1bd0f12d3bcd9fb8fab0d1139b08bc554d9a3c
delta038 <164729948+delta038@users.noreply.github.com>
2026-04-11 05:36:40 +0000

    Deploying to gh-pages from @ delta038/multilingual-translator@70acba614c09a26dd4f0df32e28af490a08b8d8e 🚀

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -51,7 +51,7 @@ if (flet.noCdn) {
 _flutter.loader.load({
     config: flutterConfig,
     serviceWorkerSettings: {
-        serviceWorkerVersion: "2814837110" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */,
+        serviceWorkerVersion: "3786637347" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */,
     },
     onEntrypointLoaded: async function (engineInitializer) {
         const engine = await engineInitializer.initializeEngine(flutterConfig);
