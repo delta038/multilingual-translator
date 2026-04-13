a452fdae331d1c0d3abb7f89def9d501d83e7a48
Ono Ryota <y.ryota38@gmail.com>
2026-04-13 13:33:56 +0900

    ダイアログから言語選択をできるように変更

--- mytranslator2.py
+++ mytranslator2.py
@@ -3,6 +3,7 @@ from googletrans import Translator
 import googletrans
 import asyncio
 from dataclasses import dataclass, field
+from collections.abc import Callable
 
 DEBUG = True
 
@@ -76,35 +77,80 @@ class Languages:
     value: set = field(default_factory=set)
 
     def append(self, target: str):
-        self.value.add(target)
+        log(f'[Languages][append] called. {target=}')
+        # self.value.addだと変更通知がUIに届かないので、丸ごと変更する
+        copied = self.value.copy()
+        copied.add(target)
+        self.value = copied.copy()
+        # self.value.add(target)
+        log(f'[Languages][append] current languages {self.value}')
 
     def delete(self, target: str):
-        self.value.discard(target)
+        log(f'[Languages][delete] called. {target=}')
+        # self.value.discardだと変更通知がUIに届かないので、丸ごと変更する
+        copied = self.value.copy()
+        copied.discard(target)
+        self.value = copied.copy()
+        # self.value.discard(target)
+        log(f'[Languages][delete] current languages {self.value}')
 
 @ft.component
-def LanguagesSelectionForm(languages: Languages) -> ft.Control:
+def LanguagesSelectionForm(lang: str, delete_from_store: Callable[[str], None]) -> ft.Control:
     log(f'[LanguagesSelectionForm] rendered.')
+
+    def delete():
+        log(f'[LanguagesSelectionForm][delete] called.')
+        delete_from_store(lang)
+
+    return ft.Row(
+                [
+                ft.Dropdown(
+                    width=220,
+                    value=lang,
+                    options=[ft.DropdownOption(key=k, text=v) for k, v in googletrans.LANGUAGES.items()]
+                ),
+                ft.IconButton(icon=ft.Icons.DELETE, on_click=lambda e: delete())
+                ]
+            )
+
+@ft.component
+def LanguagesSelectionView(languages: Languages) -> ft.Control:
+    log(f'[LanguagesSelectionView] rendered.')
     
     is_open_popup, set_is_open_popup = ft.use_state(False)
-    lang, set_lang = ft.use_state('')
+    lang_id, set_lang_id = ft.use_state('')
 
     def show_popup():
-        log(f'[LanguagesSelectionForm][show_popup] called.')
+        log(f'[LanguagesSelectionView][show_popup] called.')
         set_is_open_popup(True)
 
     def close_popup():
-        log(f'[LanguagesSelectionForm][close_popup] called.')
+        log(f'[LanguagesSelectionView][close_popup] called.')
         set_is_open_popup(False)
 
     def add():
-        log(f'[LanguagesSelectionForm][add] called.')
-        languages.append(lang)
+        log(f'[LanguagesSelectionView][add] called. {lang_id=}')
+        languages.append(lang_id)
 
     if is_open_popup:
         return ft.AlertDialog(
                 title=ft.Text('言語選択'),
-                content=ft.Row(
-                    [ft.Text('言語')]
+                content=ft.Column(
+                    [
+                        ft.Column(
+                            [LanguagesSelectionForm(lang, languages.delete) for lang in languages.value]),
+                        
+                        ft.Row(
+                            [
+                                ft.Dropdown(
+                                    value=lang_id,
+                                    options=[ft.DropdownOption(key=k, text=v) for k, v in googletrans.LANGUAGES.items()],
+                                    on_select=lambda e: set_lang_id(str(e.data)),
+                                    ),
+                                ft.IconButton(icon=ft.Icons.ADD, on_click=lambda e: add())
+                                ]
+                            )
+                     ]
                     ),
                 open=True,
                 on_dismiss=lambda e: close_popup(),
@@ -133,7 +179,7 @@ def AppView() -> list[ft.Control]:
                 width=220,
                 options=[ft.DropdownOption(key=k, text=v) for k, v in googletrans.LANGUAGES.items()]
                 ),
-            LanguagesSelectionForm(languages)
+            LanguagesSelectionView(languages)
             ]
 
 ft.run(lambda page: page.render(AppView))
