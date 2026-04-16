eee320a58dfe5a90321a8ef1796c1151b71cb35e
Ono Ryota <y.ryota38@gmail.com>
2026-04-16 20:10:14 +0900

    閉じるボタンでダイアログが閉じられるように修正

--- components/language_selection.py
+++ components/language_selection.py
@@ -46,20 +46,23 @@ def LanguagesSelectionView(languages: Languages) -> ft.Control:
 
     ft.use_effect(save, [languages.value])
 
-    def show_popup():
+    def show_popup(e):
         log(f'[LanguagesSelectionView][show_popup] called.')
         set_is_open_popup(True)
 
-    def close_popup():
+    def close_popup(e=None):
         log(f'[LanguagesSelectionView][close_popup] called.')
         set_is_open_popup(False)
 
     def add():
         log(f'[LanguagesSelectionView][add] called. {lang_id=}')
-        languages.append(lang_id)
+        if lang_id:
+            languages.append(lang_id)
 
-    if is_open_popup:
-        return ft.AlertDialog(
+    return ft.Row(
+        [
+            ft.IconButton(icon=ft.Icons.SETTINGS, on_click=show_popup),
+            ft.AlertDialog(
                 title=ft.Text('言語選択'),
                 content=ft.Column(
                     [
@@ -77,10 +80,14 @@ def LanguagesSelectionView(languages: Languages) -> ft.Control:
                                 ft.IconButton(icon=ft.Icons.ADD, on_click=lambda e: add())
                                 ]
                             )
-                     ]
+                     ],
+                    tight=True,
                     ),
-                open=True,
-                on_dismiss=lambda e: close_popup(),
+                actions=[
+                    ft.TextButton('閉じる', on_click=close_popup)
+                    ],
+                open=is_open_popup,
+                on_dismiss=close_popup,
                 )
-
-    return ft.IconButton(icon=ft.Icons.SETTINGS, on_click=lambda e: show_popup())
+        ]
+    )
